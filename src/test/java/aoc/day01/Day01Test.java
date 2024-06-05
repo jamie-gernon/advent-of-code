529adcd83bcfa03646f38b13903155711eb9524b
Jamie Gernon <jamie.gernon@autotrader.co.uk>
2024-06-05 12:03:07 +0100

    Add puzzle input and use AssertJ assertions
    Co-authored-by: Mark Crossfield <mark.crossfield@autotrader.co.uk>

--- src/test/java/aoc/day01/Day01Test.java
+++ src/test/java/aoc/day01/Day01Test.java
@@ -6,24 +6,38 @@ import org.junit.jupiter.api.Test;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.*;
 
 public class Day01Test {
 
     @Test
-    public void testPart1(){
+    public void testPart1() {
         // Given
-        List<String> input = Collections.singletonList("test");
+        List<String> input = List.of("1000",
+                "2000",
+                "3000",
+                "",
+                "4000",
+                "",
+                "5000",
+                "6000",
+                "",
+                "7000",
+                "8000",
+                "9000",
+                "",
+                "10000"
+        );
 
         // When
         String result = new Day01().part1(input);
 
         // Then
-        assertEquals(input.get(0), result);
+        assertThat(result).isEqualTo("24000");
     }
 
     @Test
-    public void testPart2(){
+    public void testPart2() {
         // Given
         List<String> input = Collections.singletonList("test");
 
@@ -31,6 +45,6 @@ public class Day01Test {
         String result = new Day01().part2(input);
 
         // Then
-        assertEquals(input.get(0), result);
+        assertThat(result).isEqualTo(input.get(0));
     }
 }
