bc380107babe4e273e41649339c5b171a8f0b4a7
jamie <jamiegernon@outlook.com>
2024-08-29 14:19:28 +0100

    Completed Day03 part 2

--- src/main/java/aoc/day03/Day03.java
+++ src/main/java/aoc/day03/Day03.java
@@ -3,10 +3,7 @@ package aoc.day03;
 import aoc.Day;
 import aoc.day02.Day02;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Day03 extends Day {
     static {
@@ -20,21 +17,59 @@ public class Day03 extends Day {
     @Override
     public String part1(List<String> input) {
 
-        int totalPriority = 0;
+        // Things about collections:
+        // List: values can go in more than once, items are ordered
+        // Set: can’t exist in a set more than once, does not have an order
+        // Map: two values can’t have the same key, the last one overwrites the first, items are not ordered
+
+        int totalPriorityPart1 = 0;
         for (String rucksack: input) {
             int middleString = rucksack.length() / 2;
             String compartment1 = rucksack.substring(0, middleString);
             String compartment2 = rucksack.substring(middleString, rucksack.length());
-            Set set1 = new HashSet<>(Arrays.asList(compartment1.split("")));
-            Set set2 = new HashSet<>(Arrays.asList(compartment2.split("")));
+            Set <String> set1 = new HashSet<>(Arrays.asList(compartment1.split("")));
+            Set <String> set2 = new HashSet<>(Arrays.asList(compartment2.split("")));
             set1.retainAll(set2);
-
+            if (set1.size() > 1) {
+                throw new IllegalStateException("There can only be one");
+            }
+            for (String item : set1) {
+                Character item2 = item.toCharArray()[0];
+                if (Character.isLowerCase(item2)) {
+                    totalPriorityPart1 += item2 -'a'+ 1;
+                } else {
+                    totalPriorityPart1 += item2 -'A'+ 27;
+                }
+            }
         }
-        return String.valueOf(totalPriority);
+        return String.valueOf(totalPriorityPart1);
     }
 
     @Override
     public String part2(List<String> input) {
-        return null;
+
+        int totalPriorityPart2 = 0;
+        for (int index = 0; index < input.size(); index += 3) {
+            String backpack1 = input.get(index);
+            String backpack2 = input.get(index + 1);
+            String backpack3 = input.get(index + 2);
+            Set <String> set1 = new HashSet<>(Arrays.asList(backpack1.split("")));
+            Set <String> set2 = new HashSet<>(Arrays.asList(backpack2.split("")));
+            Set <String> set3 = new HashSet<>(Arrays.asList(backpack3.split("")));
+            set1.retainAll(set2);
+            set1.retainAll(set3);
+            if (set1.size() > 1) {
+                throw new IllegalStateException("There can only be one");
+            }
+            for (String item : set1) {
+                Character item2 = item.toCharArray()[0];
+                if (Character.isLowerCase(item2)) {
+                    totalPriorityPart2 += item2 -'a'+ 1;
+                } else {
+                    totalPriorityPart2 += item2 -'A'+ 27;
+                }
+            }
+        }
+        return String.valueOf(totalPriorityPart2);
     }
 }
--- src/test/java/aoc/day03/Day03Test.java
+++ src/test/java/aoc/day03/Day03Test.java
@@ -1,8 +1,11 @@
 package aoc.day03;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,6 +13,61 @@ import static org.junit.jupiter.api.Assertions.*;
 class Day03Test {
     @Test
     void part1FirstRucksack() {
+        // vJrwpWtwJgWr hcsFMMfFFhFp
         assertThat(new Day03().part1(List.of("vJrwpWtwJgWrhcsFMMfFFhFp"))).isEqualTo("16");
+    }
+
+    @Test
+    void part1SecondRucksack() {
+        assertThat(new Day03().part1(List.of("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"))).isEqualTo("38");
+    }
+
+    @Test
+    void howSetsWork() {
+        Set<String> testSet = new HashSet<>();
+        testSet.add("p");
+        testSet.add("p");
+        assertThat(testSet.size()).isEqualTo(1);
+    }
+
+    @Test
+    void part2FirstGroup() {
+        assertThat(new Day03().part2(List.of(
+                "vJrwpWtwJgWrhcsFMMfFFhFp",
+                "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                "PmmdzqPrVvPwwTWBwg"
+        ))).isEqualTo("18");
+    }
+
+    @Test
+    void part2SecondGroup() {
+        assertThat(new Day03().part2(List.of(
+                "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                "ttgJtRGJQctTZtZT",
+                "CrZsJsPPZsGzwwsLwLmpwMDw"))).isEqualTo("52");
+        // The common letter is Z
+    }
+
+    @Test
+    void part2MultipleGroups() {
+        assertThat(new Day03().part2(List.of(
+                "vJrwpWtwJgWrhcsFMMfFFhFp",
+                "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                "PmmdzqPrVvPwwTWBwg",
+                "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                "ttgJtRGJQctTZtZT",
+                "CrZsJsPPZsGzwwsLwLmpwMDw"))).isEqualTo("70");
+        // The common letter is Z
+    }
+
+    @Test
+    @Disabled
+    void howIntsWork() {
+        int counter;
+        for (counter = 0; counter >= 0; counter++) {
+
+        }
+        assertThat(counter).isLessThan(0);
+        assertThat(counter).isEqualTo(0);
     }
 }
